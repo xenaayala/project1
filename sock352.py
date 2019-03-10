d399529b22ccc3cd467342cca35c4ffb503888d2
Xena Ayala <xa13@ilab1.cs.rutgers.edu>
2019-03-10 19:33:48 -0400

    Updated Connect

--- sock352.py
+++ sock352.py
@@ -46,15 +46,45 @@ class socket:
 	return 
 
     def connect(self,address):  # fill in your code here 
-        return 
+        # Three way handshake
+	#set header flags here
+        	version = 0x1       
+        	flags = SOCK352_SYN 
+        	opt_ptr = 0
+        	protocol = 0
+        	header_len = 0
+        	checksum = 0
+        	source_port = 0
+        	dest_port = 0
+        	seq_no = 0        
+        	ack_no = 0         
+        	window = 0
+        	payload_len = 0     
+        	header_size = struct.calcsize(sock352PktHdrData)
+        	header = self.udpPkt_hdr_data.pack(version, flags, opt_ptr, protocol, header_size, checksum, source_port, dest_port, seq_no, ack_no, window, payload_len)
+		csocket = self
+		
+		csocket.recv(SOCK352_SYN)
+		csocket.send(SOCK352_ACK)
+
+		self.socket.connect(address, int(UDPportRx))
+		print("Client Connected")
+
+	return 
     
     def listen(self,backlog):
-        return
+        #dont need for this project
+	return
 
     def accept(self):
         (clientsocket, address) = (1,1)  # change this to your code 
+
+
         return (clientsocket,address)
     
+
+
+
     def close(self):   # fill in your code here 
         return 
 
